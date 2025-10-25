d17bcfb52efb8c0186c3740405f13dc2676be9bd
Em Riz <mtrinh10052005@gmail.com>
2025-10-25 21:45:34 +0700

    fix ocelot

---
+++ FeedbackService/Domain/Entities/GenerateFeedbackDetail.cs
@@ -0,0 +1,16 @@
+﻿namespace FeedbackService.Domain.Entities
+{
+    public class GenerateFeedbackDetail
+    {
+        public int Id { get; set; }
+        public int GenerateFeedbackId { get; set; }     
+        public string CaseName { get; set; } = string.Empty;
+        public bool Passed { get; set; }
+        public string? Input { get; set; }
+        public string? Expected { get; set; }
+        public string? Actual { get; set; }
+        public string? Comment { get; set; }
+
+        public GeneratedFeedbackRecord? GenerateFeedback { get; set; }
+    }
+}
