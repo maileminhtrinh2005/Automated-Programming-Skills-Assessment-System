a01931d3bab07bd532417fec827e8f43d0670b1d
Em Riz <mtrinh10052005@gmail.com>
2025-11-02 22:09:13 +0700

    sua db feedback

--- FeedbackService/Application/Dtos/FeedbackAutoRequestDto.cs
+++ FeedbackService/Application/Dtos/FeedbackAutoRequestDto.cs
@@ -8,16 +8,14 @@
 
     public class AssignmentDto
     {
-        public int AssignmentId { get; set; }
         public string Title { get; set; } = string.Empty;
-        public string Description { get; set; } = string.Empty;
     }
 
     public class SubmissionDto
     {
-        public int SubmissionId { get; set; }
+    
         public int AssignmentId { get; set; }
-        public int StudentId { get; set; }
+  
         public string SourceCode { get; set; } = string.Empty;
         public int LanguageId { get; set; }
     }
